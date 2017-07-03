d2a1ec3d3194346ddf2f378a494159d97fa10c9b
varma-4 <varmadatla444@gmail.com>
2017-07-03 14:52:45 +0530

    Initial Commit

---
+++ pintrestCustomLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pintrestCustomLayout
+//
+//  Created by Mani Kanta Varma Datla on 03/07/17.
+//  Copyright © 2017 Infosys LTD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
